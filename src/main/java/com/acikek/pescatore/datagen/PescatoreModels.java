1c38b2a39b66f7d374d4744f87e2b3f38e0d5377
acikek <spiralixp@gmail.com>
2023-12-18 13:59:56 -0800

    Correct rod models

--- src/main/java/com/acikek/pescatore/datagen/PescatoreModels.java
+++ src/main/java/com/acikek/pescatore/datagen/PescatoreModels.java
@@ -1,11 +1,9 @@
 package com.acikek.pescatore.datagen;
 
-import com.acikek.pescatore.item.PescatoreItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
 
 public class PescatoreModels extends FabricModelProvider {
 
@@ -19,8 +17,5 @@ public class PescatoreModels extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-        itemModelGenerator.register(PescatoreItems.ROOKIE_ROD, Models.HANDHELD_ROD);
-        itemModelGenerator.register(PescatoreItems.ADEPT_ROD, Models.HANDHELD_ROD);
-        itemModelGenerator.register(PescatoreItems.EXPERT_ROD, Models.HANDHELD_ROD);
     }
 }
